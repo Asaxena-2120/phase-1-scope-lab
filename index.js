c87e9704b085000784a0d87f10c61b2fc3d28924
Aastha <saxena.aastha91@gmail.com>
2022-11-14 19:59:33 -0500

    commit tree 4395d5d5a649542f24f951e5d3021c3f3ac7990e

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+const leastFavoriteCustomer = "hello"
+var upperCaseCustomerName = ()=>{
+    customerName = customerName.toUpperCase();
+    }
+var setBestCustomer = () => {
+    /* Variables declared without var keyword inside any function becomes global variables automatically. */
+    bestCustomer = 'not bob';
+  
+}
+    
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob';
+var changeLeastFavoriteCustomer = () => leastFavoriteCustomer = "kiki";
